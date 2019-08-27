555b846dcbaaabd631e87f30e2d2b5b9c9fe1681
JayanthMogadampalli <jayu6766@gmail.com>
2019-08-27 15:42:27 +0530

    website with basic layout created

---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+ const Header=()=> (
+        <div>
+            <h1 className="title-cotainer__title">Weather Forecast</h1>
+            <h3 className="title-container__subtitle">Get to know climate conditions around you..</h3>
+        </div>
+ )
+
+export default Header;
